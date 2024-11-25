3bad5a7f2107c0a95113a05c38013b93531bad1a
MasonScarbro <masonscarbropf@gmail.com>
2024-11-25 00:02:06 -0500

    Added review model

---
+++ api/models/review.py
@@ -0,0 +1,25 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class ReviewBase(BaseModel):
+    review_text: str
+    score: int
+    customer_id: int  # Foreign key reference
+
+
+class ReviewCreate(ReviewBase):
+    pass
+
+
+class ReviewUpdate(BaseModel):
+    review_text: Optional[str] = None
+    score: Optional[int] = None
+    customer_id: Optional[int] = None
+
+
+class Review(ReviewBase):
+    id: int
+
+    class Config:
+        orm_mode = True
